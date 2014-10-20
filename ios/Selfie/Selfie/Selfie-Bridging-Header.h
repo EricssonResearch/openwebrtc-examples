caaddc048cb875185e040d2231e5c0d58c206338
Stefan Ålund <stefan.alund@ericsson.com>
2014-10-20 12:15:16 +0200

    Simple self-view app

---
+++ ios/Selfie/Selfie/Selfie-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#include <owr/owr.h>
+#include <owr/owr_local.h>
+#include <owr/owr_video_renderer.h>
+#include <owr/owr_window_registry.h>
