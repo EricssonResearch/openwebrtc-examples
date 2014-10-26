34ae887775f463391b7eaccf3063b7bcd6272824
Stefan Ålund <stefan.alund@ericsson.com>
2014-10-26 21:18:30 +0100

    Hybrid iOS example app
    Native app that wraps the WebRTC web app demo.openwebrtc.io

---
+++ ios/SimpleDemo/SimpleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleDemo
+//
+//  Created by Stefan Ålund on 26/10/14.
+//  Copyright (c) 2014 Ericsson Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SimpleDemoAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleDemoAppDelegate class]));
+    }
+}
