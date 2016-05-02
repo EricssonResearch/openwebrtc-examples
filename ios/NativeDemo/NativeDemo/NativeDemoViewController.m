EricssonResearch/openwebrtc-examples

commit 8f7b847b5f2d0a267ef1eaea5f8b2f405af6a120

web: Move transformation of signaling messages to the client
The server provides generic message channels between clients and should
relay messages without knowing what they contain.

Closes #44.

web/client/main.js
@@ -23,6 +23,13 @@ var chatButton;
 var chatCheckBox;
 var channel;
 
+if (!window.SDP) {
+    console.error("+-------------------------WARNING-------------------------+");
+    console.error("| sdp.js not found, will not transform signaling messages |");
+    console.error("+---------------------------------------------------------+");
+    window.SDP = { "parse": function () {}, "generate": function () {} };
+}
+
 if (!window.hasOwnProperty("orientation"))
     window.orientation = -90;
 
@@ -161,11 +168,14 @@ window.onload = function () {
 function handleMessage(evt) {
     var message = JSON.parse(evt.data);
 
-    if (!pc && (message.sdp || message.candidate))
+    if (!pc && (message.sessionDescription || message.sdp || message.candidate))
         start(false);
 
-    if (message.sdp) {
-        var desc = new RTCSessionDescription(message.sdp);
+    if (message.sessionDescription || message.sdp) {
+        var desc = new RTCSessionDescription({
+            "sdp": SDP.generate(message.sessionDescription) || message.sdp,
+            "type": message.type
+        });
         pc.setRemoteDescription(desc, function () {
             // if we received an offer, we need to create an answer
             if (pc.remoteDescription.type == "offer")
@@ -174,8 +184,25 @@ function handleMessage(evt) {
     } else if (!isNaN(message.orientation) && remoteView) {
         var transform = "rotate(" + message.orientation + "deg)";
         remoteView.style.transform = remoteView.style.webkitTransform = transform;
-    } else
+    } else {
+        var d = message.candidate.candidateDescription;
+        if (d && !message.candidate.candidate) {
+            message.candidate.candidate = "candidate:" + [
+                d.foundation,
+                d.componentId,
+                d.transport,
+                d.priority,
+                d.address,
+                d.port,
+                "typ",
+                d.type,
+                d.relatedAddress && ("raddr " + d.relatedAddress),
+                d.relatedPort && ("rport " + d.relatedPort),
+                d.tcpType && ("tcptype " + d.tcpType)
+            ].filter(function (x) { return x; }).join(" ");
+        }
         pc.addIceCandidate(new RTCIceCandidate(message.candidate), function () {}, logError);
+    }
 }
 
 // call start() to initiate
@@ -186,7 +213,18 @@ function start(isInitiator) {
     // send any ice candidates to the other peer
     pc.onicecandidate = function (evt) {
         if (evt.candidate) {
-            peer.send(JSON.stringify({ "candidate": evt.candidate }));
+            var candidate = "";
+            var s = SDP.parse("m=application 0 NONE\r\na=" + evt.candidate.candidate + "\r\n");
+            var candidateDescription = s && s.mediaDescriptions[0].ice.candidates[0];
+            if (!candidateDescription)
+                candidate = evt.candidate.candidate;
+            peer.send(JSON.stringify({
+                "candidate": {
+                    "candidate": candidate,
+                    "candidateDescription": candidateDescription,
+                    "sdpMLineIndex": evt.candidate.sdpMLineIndex
+                }
+            }));
             console.log("candidate emitted: " + evt.candidate.candidate);
         }
     };
@@ -232,8 +270,21 @@ function start(isInitiator) {
 
 function localDescCreated(desc) {
     pc.setLocalDescription(desc, function () {
-        peer.send(JSON.stringify({ "sdp": pc.localDescription }));
-        console.log("localDescription set and sent to peer, type: " + pc.localDescription.type + ", sdp: " + pc.localDescription.sdp);
+        var sdp = "";
+        var sessionDescription = SDP.parse(pc.localDescription.sdp);
+        if (!sessionDescription)
+            sdp = pc.localDescription.sdp;
+        peer.send(JSON.stringify({
+            "sdp": sdp,
+            "sessionDescription": sessionDescription,
+            "type": pc.localDescription.type
+        }));
+        var logMessage = "localDescription set and sent to peer, type: " + pc.localDescription.type;
+        if (sdp)
+            logMessage += ", sdp:\n" + sdp;
+        if (sessionDescription)
+            logMessage += ", sessionDescription:\n" + JSON.stringify(sessionDescription, null, 2);
+        console.log(logMessage);
     }, logError);
 }
 

web/channel_server.js
@@ -1,13 +1,6 @@
 var fs = require("fs");
 var http = require("http");
 var path = require("path");
-try {
-    var SDP = require("./sdp");
-} catch (e) {
-    console.error("+-------------------------WARNING-------------------------+");
-    console.error("| sdp.js not found, will not transform signaling messages |");
-    console.error("+---------------------------------------------------------+");
-}
 
 var sessions = {};
 var usersInSessionLimit = 2;
@@ -116,50 +109,6 @@ var server = http.createServer(function (request, response) {
             request.on("end", function () {
                 console.log("@" + sessionId + " - " + userId + " => " + peerId + " :");
                 // console.log(body);
-                try {
-                    if (SDP) {
-                        var message = JSON.parse(body);
-                        if (message.sdp && !message.sessionDescription) {
-                            message.sessionDescription = SDP.parse(message.sdp.sdp);
-                            message.type = message.sdp.type;
-                        } else if (message.sessionDescription && message.type && !message.sdp) {
-                            message.sdp = {
-                                type: message.type,
-                                sdp: SDP.generate(message.sessionDescription),
-                            };
-                        } else if (message.candidate) {
-                            var candidate = message.candidate;
-                            if (candidate.candidate && !candidate.candidateDescription) {
-                                var candidateInfo = SDP.parse("m=application 0 NONE\r\na=" + candidate.candidate + "\r\n");
-
-                                if (candidateInfo.mediaDescriptions[0]
-                                    && candidateInfo.mediaDescriptions[0].ice
-                                    && candidateInfo.mediaDescriptions[0].ice.candidates) {
-                                    candidate.candidateDescription = candidateInfo.mediaDescriptions[0].ice.candidates[0];
-                                }
-                            } else if (candidate.candidateDescription && !candidate.candidate) {
-                                var description = candidate.candidateDescription;
-                                candidate.candidate = 'candidate:' + [
-                                    description.foundation,
-                                    description.componentId,
-                                    description.transport,
-                                    description.priority,
-                                    description.address,
-                                    description.port,
-                                    'typ',
-                                    description.type,
-                                    description.relatedAddress && ('raddr ' + description.relatedAddress),
-                                    description.relatedPort && ('rport ' + description.relatedPort),
-                                    description.tcpType && ('tcptype ' + description.tcpType),
-                                ].filter(function (x) { return x; }).join(' ');
-                            }
-                        }
-                        body = JSON.stringify(message);
-                        // console.log(body)
-                    }
-                } catch (err) {
-                    console.error("failed to parse message: " + err)
-                }
                 var evtdata = "data:" + body.replace(/\n/g, "\ndata:") + "\n";
                 peer.esResponse.write("event:user-" + userId + "\n" + evtdata + "\n");
             });

web/client/sdp.js
@@ -1,0 +1,1 @@
+../../../openwebrtc/bridge/client/sdp.js

web/client/webrtc_example.html
@@ -5,6 +5,7 @@
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
         <link rel="stylesheet" type="text/css" href="main.css">
         <script src="signaling_channel.js"></script>
+        <script src="sdp.js"></script>
         <script src="main.js"></script>
     </head>
     <body>

web/sdp.js
@@ -1,1 +1,0 @@
-../../openwebrtc/bridge/client/sdp.js

commit f6a160e4e8a72ff6e678043739579adbdc08f073

web, NativeDemo: fix signalling
Signaling was broken by 8f7b847b5f2d0a267ef1eaea5f8b2f405af6a120. This commit
makes it work again, fixing a bug in the js signalling code and updating
NativeDemo to work with the new message formats expected by the signaling
server.

ios/NativeDemo/NativeDemo/NativeDemoViewController.m
@@ -200,8 +200,7 @@
 {
     NSLog(@"Answer generated: \n%@", answer);
 
-    NSDictionary *d = @{@"sdp": answer};
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:d
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:answer
                                                        options:NSJSONWritingPrettyPrinted
                                                          error:nil];
     NSString *answerString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
@@ -215,8 +214,7 @@
 {
     NSLog(@"Offer generated: \n%@", offer);
 
-    NSDictionary *d = @{@"sdp": offer};
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:d
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:offer
                                                        options:NSJSONWritingPrettyPrinted
                                                          error:nil];
     NSString *offerString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
@@ -230,8 +228,12 @@
 {
     NSLog(@"Candidate generated: \n%@", candidate);
     if (self.peerID) {
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:@{@"candidate": @{@"candidate": candidate}}
+                                                           options:NSJSONWritingPrettyPrinted
+                                                             error:nil];
+        NSString *candidateJson = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
         NSLog(@"Sending candidate to peer: %@", self.peerID);
-        [self.peerServer sendMessage:candidate toPeer:self.peerID];
+        [self.peerServer sendMessage:candidateJson toPeer:self.peerID];
     }
 }
 

ios/NativeDemo/NativeDemo/PeerServerHandler.m
@@ -104,9 +104,9 @@
         if (json[@"sdp"]) {
             NSString *type = json[@"type"];
             if ([@"offer" isEqualToString:type]) {
-                [self.delegate peerServer:self peer:peerUser sentOffer:json[@"sdp"][@"sdp"]];
+                [self.delegate peerServer:self peer:peerUser sentOffer:json[@"sdp"]];
             } else if ([@"answer" isEqualToString:type]) {
-                [self.delegate peerServer:self peer:peerUser sentAnswer:json[@"sdp"][@"sdp"]];
+                [self.delegate peerServer:self peer:peerUser sentAnswer:json[@"sdp"]];
             } else {
                 NSLog(@"[PeerServerHandler] WARNING! Got malformed offer/answer from peer");
             }

web/client/main.js
@@ -172,10 +172,7 @@ function handleMessage(evt) {
         start(false);
 
     if (message.sessionDescription || message.sdp) {
-        var desc = new RTCSessionDescription({
-            "sdp": SDP.generate(message.sessionDescription) || message.sdp,
-            "type": message.type
-        });
+        var desc = new RTCSessionDescription(message);
         pc.setRemoteDescription(desc, function () {
             // if we received an offer, we need to create an answer
             if (pc.remoteDescription.type == "offer")